e637aa6964952455e83e105d4ae82d3ea8ad61cf
dan-fu2004 <dan.fu@mail.utoronto.ca>
2024-01-26 03:34:02 -0500

    Added functionality to upload to gcloud from local files and fixed an error within  save_to_local function

--- manga_download_local.py
+++ manga_download_local.py
@@ -1,4 +1,5 @@
 import os
+from concurrent.futures import ThreadPoolExecutor
 from manga_collector import *
 
 def download_manga_local(chapter_values, manga_name, client_id, client_secret, refresh_token, base_directory):
@@ -29,12 +30,12 @@ def download_manga_local(chapter_values, manga_name, client_id, client_secret, r
                     hash = data['chapter']['hash']
                     panels_list = data['chapter']['data']
 
-                for panel_num, panel in enumerate(panels_list):
-                    type = panel.split('.')[-1]
-                    final_name = base_file_name.format(manga_name=manga_name, language=language, chapter=chapter, panel=pad_number(str(panel_num)),type=type)
-                    final_path = os.path.join(base_directory, final_name)
-                    future = executor.submit(save_to_local, base + "/data/" + hash + "/" + panel, final_path, bearer_token, refresh_token, client_id, client_secret)
-                    futures.append(future)
+                    for panel_num, panel in enumerate(panels_list):
+                        type = panel.split('.')[-1]
+                        final_name = base_file_name.format(manga_name=manga_name, language=language, chapter=chapter, panel=pad_number(str(panel_num)),type=type)
+                        final_path = os.path.join(base_directory, final_name)
+                        future = executor.submit(save_to_local, base + "/data/" + hash + "/" + panel, final_path, bearer_token, refresh_token, client_id, client_secret)
+                        futures.append(future)
 
         for future in futures:
             future.result()
@@ -56,3 +57,58 @@ def save_to_local(url, file_path, bearer_token, refresh_token, client_id, client
                 time.sleep(7)
         except:
             time.sleep(6)
+
+def upload_from_local_file(upload_name, local_file, bucket):
+    # Function to uplaod to GCS from a local folder. Note each folder here represents 1 chapter and contains the panels for that chapter
+    # We will use threading later on to upload multiple
+    blob = bucket.blob(upload_name)
+    blob.content_type = 'image/png'
+
+    max_tries = 10
+    start = 1
+    while start < max_tries:
+        try:
+            blob.upload_from_filename(local_file)
+            break
+        except:
+            time.sleep(2**start)
+        start+=1
+
+def upload_manga(bucket_name, manga_name):
+    client = storage.Client()
+    bucket = client.bucket(bucket_name)
+    base_dir = "/Users/danfu/Downloads/"
+
+    manga_dir = os.path.join(base_dir, manga_name)
+
+    try:
+        languages = [d for d in os.listdir(manga_dir) if d != ".DS_Store"]
+    except FileNotFoundError:
+        print(f"Directory {manga_dir} not found.")
+        return
+
+    for language in languages:
+        language_dir = os.path.join(manga_dir, language)
+        
+        try:
+            chapters = [c for c in os.listdir(language_dir) if c != ".DS_Store"]
+        except FileNotFoundError:
+            print(f"Directory {language_dir} not found.")
+            continue
+
+        with ThreadPoolExecutor(max_workers=10) as executor:
+            futures = []
+
+            for chapter in chapters:
+                chapter_dir = os.path.join(language_dir, chapter)
+                panels = [p for p in os.listdir(chapter_dir) if p != ".DS_Store"]
+                print(f"Starting upload for {manga_name} in {language}, chapter: {chapter}")
+                for panel in panels:
+                    local_file_path = os.path.join(chapter_dir, panel)
+                    gcs_path = f"{manga_name}/{language}/{chapter}/{panel}"
+      
+                    future = executor.submit(upload_from_local_file, gcs_path, local_file_path, bucket)
+                    futures.append(future)
+
+            for future in futures:
+                future.result()
